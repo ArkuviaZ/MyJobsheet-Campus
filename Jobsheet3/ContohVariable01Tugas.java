9a7b087f75481d460e47a17f0e3b39bf9f9aab1c
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-06 22:30:33 +0700

    Jobsheet 1.0

---
+++ Jobsheet3/ContohVariable01Tugas.java
@@ -0,0 +1,14 @@
+public class ContohVariable01Tugas{
+    public static void main(String[] args){
+        String hoby = "Bermain Petak Umpet";
+        boolean pandai = true;
+        char jk = 'L';
+        byte _umur = 19;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(hoby);
+        System.out.println("Apakah pandai? " + pandai);
+        System.out.println("Jenis kelamin: " + jk);
+        System.out.println("Umurku saat ini: " + _umur);
+        System.out.println(String.format("Saya berisk %s , dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
