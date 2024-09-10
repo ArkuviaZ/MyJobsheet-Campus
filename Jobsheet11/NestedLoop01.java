16c34fcef09403375e941e9e7ec72059e6dfd457
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-10 22:34:36 +0700

    1.2.11.3

--- Jobsheet11/NestedLoop01.java
+++ Jobsheet11/NestedLoop01.java
@@ -3,10 +3,28 @@ import java.util.Scanner;
 public class NestedLoop01 {
     public static void main(String[] args) {
         //deklarasi
-        Scanner sc = new Scanner(System.in);
-        int[][] lemps = new int[5][7];
+        Scanner scanner = new Scanner(System.in);
+        int[][] temps = new int[5][7];
         //eksekusi
-        if
+        for (int i=0; i<temps.length; i++){
+            System.out.println("Kota ke-"+i);
+            for (int j = 0; j < temps.length; j++) {
+                System.out.println("Hari ke-" + (j+1)+": ");
+                temps[i][j] = (int) scanner.nextDouble();
+            }
+            System.out.println();
+        }
+        /*
+        System.out.println("=======================");
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < temps.length; j++) {
+                System.out.println("Hari ke-" + j);
+                temps[i][j] = (int) scanner.nextDouble();
+            }
+            System.out.println();
+        }
+        */
     }
 }
 
