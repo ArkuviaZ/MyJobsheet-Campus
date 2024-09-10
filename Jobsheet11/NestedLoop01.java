2bdb362e7ca7a3405a32736c0313049ac0967ca6
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-10 22:18:13 +0700

    1.2.10.3

--- Jobsheet11/NestedLoop01.java
+++ Jobsheet11/NestedLoop01.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
-public class Tempelate {
+public class NestedLoop01 {
     public static void main(String[] args) {
         //deklarasi
         Scanner sc = new Scanner(System.in);
+        int[][] lemps = new int[5][7];
+        //eksekusi
+        if
     }
 }
 
--- Jobsheet11/Square01.java
+++ Jobsheet11/Square01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Star01 {
+public class Square01 {
     public static void main(String[] args) {
         //deklarasi
         Scanner sc = new Scanner(System.in);
@@ -8,8 +8,11 @@ public class Star01 {
         System.out.print("Masukkan Nilai N =");
         int N = sc.nextInt();
         //eksekusi
-        for(int i=1; i<=N; i++){
-            System.out.print("*");
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
