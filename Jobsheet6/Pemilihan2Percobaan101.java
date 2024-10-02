32693d865c7e4fa545033caa0b45c14433aba7c0
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-10-02 16:12:14 +0700

    1.5.11.3

--- Jobsheet6/Pemilihan2Percobaan101.java
+++ Jobsheet6/Pemilihan2Percobaan101.java
@@ -10,9 +10,15 @@ public class Pemilihan2Percobaan101 {
         tahun = absen01.nextInt();
         //eksekusi + output
         if ((tahun%4)==0){
-            if ((tahun%100)==0) {System.out.println(" Bukan Tahun Kabisat");}
-            else {System.out.println("Tahun Kabisat");}
+            if ((tahun%100)!=0) {
+                System.out.println("Tahun Kabisat");
+                }
+            else {
+                System.out.println("Bukan Tahun Kabisat");
+                }
         }
-        else {System.out.println("Bukan Tahun Kabisat");}
+        else {
+            System.out.println("Bukan Tahun Kabisat");
+            }
     }
 }
--- Jobsheet6/Pemilihan2Percobaan201.java
+++ Jobsheet6/Pemilihan2Percobaan201.java
@@ -1,29 +1,99 @@
+
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan201 {
+
     public static void main(String[] args) {
-        //deklarasi
+        //scanner
         Scanner absen01 = new Scanner(System.in);
-        float sudut1, sudut2, sudut3, totalSudut;
+
+        //deklarasi variabel
+        String member, qris;
+        int jumlah_beli, potongan = 1000, menu;
+        double total_bayar = 0, diskon, harga = 0;
+
         //input
-        System.out.print("Masukkan Besar Sudut 1: ");
-        sudut1 = absen01.nextInt();
-        System.out.print("Masukkan Besar Sudut 2: ");
-        sudut2 = absen01.nextInt();
-        System.out.print("Masukkan Besar Sudut 3: ");
-        sudut3 = absen01.nextInt();
-        //eksekusi
-        totalSudut = sudut1 + sudut2 + sudut3;
-        if(totalSudut==180){
-            if ((sudut1==90) || (sudut2==90) || (sudut3==90)){
-                System.err.println("Segitiga tersebut adalah segitiga siku-siku");
+        System.err.println("============================");
+        System.out.println("\tMenu Cafe 1A");
+        System.err.println("============================");
+        System.out.println("1. Rice Bowl");
+        System.out.println("2. Ice Tea");
+        System.out.println("3. Paket Bunding (Rice Bowl + Ice Tea)");
+        System.out.println("============================");
+        System.out.print("Masukkan Menu: ");
+        menu = absen01.nextInt();
+        System.out.print("Masukkan Jumlah: ");
+        jumlah_beli = absen01.nextInt();
+        absen01.nextLine();
+        System.out.print("Punya Member (y/t) ? ");
+        member = absen01.nextLine().toLowerCase();
+        System.out.print("Apakah menggunakan Qris (y/t) ? ");
+        qris = absen01.nextLine().toLowerCase();
+
+        //controlstatement
+        if (qris.equals("y")) {
+            if (member.equals("y")) {
+                diskon = 0.1;
+                if (menu == 1) {
+                    harga = 14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                } else if (menu == 2) {
+                    harga = 3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                } else if (menu == 3) {
+                    harga = 15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
+                }
+
+                //menghitung total bayar dengan diskon
+                total_bayar = harga - (harga * diskon) * jumlah_beli - potongan;
+            } else {
+                if (menu == 1) {
+                    harga = 14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                } else if (menu == 2) {
+                    harga = 3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                } else if (menu == 3) {
+                    harga = 15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
+                }
+                total_bayar = harga * jumlah_beli - potongan;
             }
-            else {
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        } else {
+            if (member.equals("y")) {
+                diskon = 0.1;
+                if (menu == 1) {
+                    harga = 14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                } else if (menu == 2) {
+                    harga = 3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                } else if (menu == 3) {
+                    harga = 15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
+                }
+
+                //menghitung total bayar dengan diskon
+                total_bayar = harga - (harga * diskon) * jumlah_beli;
+            } else {
+                if (menu == 1) {
+                    harga = 14000;
+                    System.out.println("Harga RIce Bowl: " + harga);
+                } else if (menu == 2) {
+                    harga = 3000;
+                    System.out.println("Harga Ice Tea: " + harga);
+                } else if (menu == 3) {
+                    harga = 15000;
+                    System.out.println("Harga Paket Bunding (Rice Bowl + Ice Tea): " + harga);
+                }
+                total_bayar = harga * jumlah_beli;
             }
         }
-        else {
-            System.out.println("Bukan Segitiga");
-        }
+        //output
+        System.err.println("============================");
+        System.out.println("\tTotal Bayar");
+        System.err.println("============================");
+        System.out.println("Total Bayar Sebesar " + total_bayar);
     }
 }
